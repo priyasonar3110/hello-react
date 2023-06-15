099aa82ea19629c04e2fa3b82897d4ddbfa5e90c
unknown <priyasonar.2009@gmail.com>
2023-06-15 11:04:03 +0530

    nested syntax react

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 /*
 <div id="parent">
-    <div id="child">
+    <div id="child1">
+        <h1 id="h1tag">I am h1 tag</h1>
+        <h1 id="h2tag">I am h2 tag</h1>
+    </div>
+      <div id="child2">
         <h1 id="h1tag">I am h1 tag</h1>
         <h1 id="h2tag">I am h2 tag</h1>
     </div>
@@ -14,8 +18,9 @@ const h2tag = React.createElement("h1", {
 const h1Tag = React.createElement("h1", {
     id: "h1tag"
 }, "I am h1 tag");
-const child = React.createElement("div", { id: "child" }, [h1Tag, h2tag]);
-const parent = React.createElement("div", { id: "parent" }, child);
+const child1 = React.createElement("div", { id: "child1" }, [h1Tag, h2tag]);
+const child2 = React.createElement("div", { id: "child2" }, [h1Tag, h2tag]);
+const parent = React.createElement("div", { id: "parent" }, [child1, child2]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
